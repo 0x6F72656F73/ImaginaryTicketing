250f2803312775a36fb2466ee4c510aa5fb1f0dd
GudOreos <52138736+0x6F72656F73@users.noreply.github.com>
2021-08-15 16:12:55 -0700

    🔊 feat(bg): added logging after tasks finish

--- src/cogs/tasks.py
+++ src/cogs/tasks.py
@@ -1,9 +1,12 @@
+import logging
+
 from discord.ext import commands
 import aiocron
 
 from utils.background import AutoClose, ScrapeChallenges, UpdateHelpers
 from utils import exceptions
 
+log = logging.getLogger(__name__)
 class Tasks(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -13,26 +16,30 @@ class Tasks(commands.Cog):
         async def start_auto_close():
             await AutoClose.main(self.bot, hours=48)
             # await AutoClose.main(self.bot, seconds=2)
+            log.info("Finished Task AutoClose")
 
         @aiocron.crontab("0 9 * * *")
         async def start_scraping_challenges_9():
             await ScrapeChallenges.main(self.bot)
+            log.info("Finished Task ScrapeChallenges at 9 AM")
 
         @aiocron.crontab("30 9 * * *")
         async def start_scraping_challenges_9_30():
             await ScrapeChallenges.main(self.bot)
+            log.info("Finished Task ScrapeChallenges at 9 30 AM")
 
         @aiocron.crontab("0 */2 * * *")
         async def start_scraping_challenges_2_hours():
             await ScrapeChallenges.main(self.bot)
+            log.info("Finished Task ScrapeChallenges for every 2 hours")
 
-        # @aiocron.crontab("*/10 * * *")
-        # @aiocron.crontab("* * * * * */10")
+        @aiocron.crontab("*/10 * * *")
         async def start_adding_users():
             try:
                 await UpdateHelpers.main(self.bot)
             except exceptions.ChallengeDoesNotExist:
                 await ScrapeChallenges.main(self.bot)
+            log.info("Finished Task UpdateHelpers for every 10 minutes")
 
 
 def setup(bot):
